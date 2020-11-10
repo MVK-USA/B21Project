2bf734d4b4ae38eacdb1174fb726fa7678043625
MVK-USA <mvk2015@yahoo.com>
2020-11-10 12:49:14 -0500

    PROJECT IS SHARED, commit and push

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,8 @@
 public class Hello {
     // more stuff
     // Mac user and windows
+
+    // I have already published on GitHub.com
+    // Do I need to share again?
+    // It's time to commit and PUSH
 }
